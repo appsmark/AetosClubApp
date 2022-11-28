076b85f37ccade2cc2916dfc818c4461340df6d6
appsmark <martin.keesen@gmail.com>
2022-11-28 11:24:39 +0100

    Deploy 2.1 for Android

--- res/deployAndroid.bat
+++ res/deployAndroid.bat
@@ -1,2 +1,1 @@
-ns clean
 ns build android --release --key-store-path res/aetos.keystore --key-store-password AppsMark --key-store-alias aetos --key-store-alias-password AppsMark --copy-to res/aetos.apk
